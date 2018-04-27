ae623eda77ab765ac0ed15f3bfd895808aa265a9
MarkusBuechler <markus.buechler@mail.de>
2018-04-27 20:54:16 +0200

    added more sample tests

--- app/src/test/java/de/htwg/margogo/monstermaths/ExampleUnitTest.java
+++ app/src/test/java/de/htwg/margogo/monstermaths/ExampleUnitTest.java
@@ -11,7 +11,37 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void addition_isCorrectA() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void addition_isCorrecB() {
+        assertEquals(6, 3 + 3);
+    }
+
+    @Test
+    public void addition_isCorrecC() {
+        assertEquals(8, 2 + 6);
+    }
+
+    @Test
+    public void addition_isCorrectD() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subtraction_isCorrectA() {
+        assertEquals(4, 8 - 4);
+    }
+
+    @Test
+    public void subtraction_isCorrectB() {
+        assertEquals(4, 6 - 2);
+    }
+
+    @Test
+    public void subtraction_isCorrectC() {
+        assertEquals(1, 3 - 2);
     }
 }

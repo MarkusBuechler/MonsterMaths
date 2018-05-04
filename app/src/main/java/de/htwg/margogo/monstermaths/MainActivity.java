b0c75544d5baa47e65c5aa01f935f9d2809df034
MarkusBuechler <markus.buechler@mail.de>
2018-05-04 20:28:07 +0200

    removed sensor stuff, added new activity and connected both

--- app/src/main/java/de/htwg/margogo/monstermaths/MainActivity.java
+++ app/src/main/java/de/htwg/margogo/monstermaths/MainActivity.java
@@ -1,6 +1,8 @@
 package de.htwg.margogo.monstermaths;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,77 +10,37 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends Activity {
 
-    private SensorManager mSensorManager;
-    private Sensor mGyroskop;
+    private Button btnStartGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnStartGame = findViewById(R.id.btnStartGame);
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Gehe zum nächsten Screen!",
+                        Toast.LENGTH_LONG).show();
 
-        Log.i("MyLog", "Sample log entry");
+                // Create intent and start activity
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                //intent.putExtra("key", targetGoal);
+                startActivity(intent);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        assert mSensorManager != null;
-        mGyroskop = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        if (checkSensorAvaibility(Sensor.TYPE_GYROSCOPE)) {
-            // sensor available
-            // you can play the game
-            Log.i("MyLog", "Gyroscope sensor is available");
-        } else {
-            //user cannot play the game
-            // should filter the devices in the playstore like this ..
-            // https://developer.android.com/guide/topics/sensors/sensors_overview
-            //<uses-feature android:name="android.hardware.sensor.gyroscope"
-            //android:required="true" />
-            Log.i("MyLog", "Gyroscope sensor is NOT available");
-        }
-
-        // asserts does not work. maybe because they lower 2 are always false and upper 2 are correct. Need output !
-        assert mSensorManager != null;
-        assert mGyroskop != null;
-        assert mSensorManager == null;
-        assert mGyroskop == null;
+            }
+        });
 
     }
 
-    private boolean checkSensorAvaibility(int sensorType) {
-        return mSensorManager.getDefaultSensor(sensorType) != null;
-    }
 
-    @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
-    }
-
-    @Override
-    public final void onSensorChanged(SensorEvent event) {
-
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-        Log.i("MyLog", "x:" + x);
-        Log.i("MyLog", "y:" + y);
-        Log.i("MyLog", "z:" + z);
-        // Do something with this sensor value.
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mGyroskop, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
 
 
 }

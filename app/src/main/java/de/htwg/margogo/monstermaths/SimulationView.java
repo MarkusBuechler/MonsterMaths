5b4689b9f9a97cdb736549bf52515be57e20cb7d
MarkusBuechler <markus.buechler@mail.de>
2018-05-10 00:22:56 +0200

    amazing work: added monster in simulationview with predefined rotation

--- app/src/main/java/de/htwg/margogo/monstermaths/SimulationView.java
+++ app/src/main/java/de/htwg/margogo/monstermaths/SimulationView.java
@@ -12,10 +12,12 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 /**
  * This is an highly adopted example of using the accelerometer to integrate the device's
@@ -27,7 +29,7 @@ import android.widget.FrameLayout;
  */
 class SimulationView extends FrameLayout implements SensorEventListener {
     // diameter of the balls in meters
-    private static final float sBallDiameter = 0.004f;
+    private static final float sBallDiameter = 0.010f;
     private static final float sBallDiameter2 = sBallDiameter * sBallDiameter;
 
     private GameActivity accelerometerPlayActivity;
@@ -48,6 +50,9 @@ class SimulationView extends FrameLayout implements SensorEventListener {
     private float mHorizontalBound;
     private float mVerticalBound;
     private final ParticleSystem mParticleSystem;
+    private float counter = 0;
+
+
     /*
      * Each of our particle holds its previous and current position, its
      * acceleration. for added realism each particle has its own friction
@@ -58,9 +63,11 @@ class SimulationView extends FrameLayout implements SensorEventListener {
         private float mPosY = (float) Math.random();
         private float mVelX;
         private float mVelY;
+        private int i;
 
-        public Particle(Context context) {
-            super(context);
+        public Particle(Context context, int i) {
+            super(context); // TODO: should be user later maybe, or use unique lists
+            this.i = i;
         }
 
         public Particle(Context context, AttributeSet attrs) {
@@ -89,6 +96,30 @@ class SimulationView extends FrameLayout implements SensorEventListener {
             mVelY += ay * dT;
         }
 
+        public void computePhysicsMonster(float sx, float sy, float dT) {
+
+            counter = counter % 180;
+            counter++;
+
+            dT = 0.01f;
+
+            final float ax = -0.5f/5;
+            final float ay = -0/5;
+
+
+            if (counter > 90) {
+                mPosX -= mVelX * dT - ax * dT * dT / 2;
+                mVelX -= ax * dT;
+            } else {
+                mPosX += mVelX * dT + ax * dT * dT / 2;
+                mVelX += ax * dT;
+            }
+
+            mPosY += mVelY * dT + ay * dT * dT / 2;
+            mVelY += ay * dT;
+
+        }
+
         /*
          * Resolving constraints and collisions with the Verlet integrator
          * can be very simple, we simply need to move a colliding or
@@ -100,6 +131,7 @@ class SimulationView extends FrameLayout implements SensorEventListener {
             final float ymax = mVerticalBound;
             final float x = mPosX;
             final float y = mPosY;
+
             if (x > xmax) {
                 mPosX = xmax;
                 mVelX = 0;
@@ -122,18 +154,30 @@ class SimulationView extends FrameLayout implements SensorEventListener {
      */
     class ParticleSystem {
         static final int NUM_PARTICLES = 1;
+        static final int NUM_MONSTERS = 1;
         private Particle mBalls[] = new Particle[NUM_PARTICLES];
+        private Particle mMonsters[] = new Particle[NUM_MONSTERS];
 
         ParticleSystem() {
             /*
              * Initially our particles have no speed or acceleration
              */
             for (int i = 0; i < mBalls.length; i++) {
-                mBalls[i] = new Particle(getContext());
+                mBalls[i] = new Particle(getContext(), 1);
                 mBalls[i].setBackgroundResource(R.drawable.emoji_smile_256);
                 mBalls[i].setLayerType(LAYER_TYPE_HARDWARE, null);
                 addView(mBalls[i], new ViewGroup.LayoutParams(mDstWidth, mDstHeight));
             }
+
+            for (int i = 0; i < mMonsters.length; i++) {
+                mMonsters[i] = new Particle(getContext(),2);
+                mMonsters[i].mPosX = 0.0275f;
+                mMonsters[i].mPosY = -0.03f;
+                mMonsters[i].setBackgroundResource(R.drawable.blue_monster_128);
+                mMonsters[i].setLayerType(LAYER_TYPE_HARDWARE, null);
+                addView(mMonsters[i], new ViewGroup.LayoutParams(mDstWidth, mDstHeight));
+            }
+
         }
 
         /*
@@ -141,6 +185,9 @@ class SimulationView extends FrameLayout implements SensorEventListener {
          * Verlet integrator.
          */
         private void updatePositions(float sx, float sy, long timestamp) {
+
+            // sx:: links + 5-10, rechts, -5-10
+            //sy :: senkrecht nach vorne 10, rückseite vorne -10
             final long t = timestamp;
             if (mLastT != 0) {
                 final float dT = (float) (t - mLastT) / 3000.f /** (1.0f / 3000000000.0f)*/;
@@ -149,6 +196,11 @@ class SimulationView extends FrameLayout implements SensorEventListener {
                         Particle ball = mBalls[i];
                         ball.computePhysics(sx, sy, dT);
                     }
+                    final int count2 = mMonsters.length;
+                    for (int i = 0; i < count2; i++) {
+                    Particle ball = mMonsters[i];
+                    ball.computePhysicsMonster(sx, sy, dT);
+                }
             }
             mLastT = t;
         }
@@ -160,7 +212,9 @@ class SimulationView extends FrameLayout implements SensorEventListener {
          */
         public void update(float sx, float sy, long now) {
             // update the system's positions
+            //updateMonsters(sx, sy, now);
             updatePositions(sx, sy, now);
+            //updateMonsters(sx, sy, now);
 
             // We do no more than a limited number of iterations
             final int NUM_MAX_ITERATIONS = 10;
@@ -173,6 +227,7 @@ class SimulationView extends FrameLayout implements SensorEventListener {
              */
             boolean more = true;
             final int count = mBalls.length;
+            final int count2 = mMonsters.length;
             for (int k = 0; k < NUM_MAX_ITERATIONS && more; k++) {
                 more = false;
                 for (int i = 0; i < count; i++) {
@@ -205,7 +260,39 @@ class SimulationView extends FrameLayout implements SensorEventListener {
                     }
                     curr.resolveCollisionWithBounds();
                 }
+
+                for (int i = 0; i < count2; i++) {
+                    Particle curr = mMonsters[i];
+                    for (int j = i + 1; j < count2; j++) {
+                        Particle ball = mMonsters[j];
+                        float dx = ball.mPosX - curr.mPosX;
+                        float dy = ball.mPosY - curr.mPosY;
+                        float dd = dx * dx + dy * dy;
+                        // Check for collisions
+                        if (dd <= sBallDiameter2) {
+                            /*
+                             * add a little bit of entropy, after nothing is
+                             * perfect in the universe.
+                             */
+                            dx += ((float) Math.random() - 0.5f) * 0.0001f;
+                            dy += ((float) Math.random() - 0.5f) * 0.0001f;
+                            dd = dx * dx + dy * dy;
+                            // simulate the spring
+                            final float d = (float) Math.sqrt(dd);
+                            final float c = (0.5f * (sBallDiameter - d)) / d;
+                            final float effectX = dx * c;
+                            final float effectY = dy * c;
+                            curr.mPosX -= effectX;
+                            curr.mPosY -= effectY;
+                            ball.mPosX += effectX;
+                            ball.mPosY += effectY;
+                            more = true;
+                        }
+                    }
+                    curr.resolveCollisionWithBounds();
+                }
             }
+
         }
 
         public int getParticleCount() {
@@ -218,6 +305,18 @@ class SimulationView extends FrameLayout implements SensorEventListener {
 
         public float getPosY(int i) {
             return mBalls[i].mPosY;
+        }
+
+        public int getParticleCount2() {
+            return mMonsters.length;
+        }
+
+        public float getPosX2(int i) {
+            return mMonsters[i].mPosX;
+        }
+
+        public float getPosY2(int i) {
+            return mMonsters[i].mPosY;
         }
     }
 
@@ -329,6 +428,19 @@ class SimulationView extends FrameLayout implements SensorEventListener {
             final float y = yc - particleSystem.getPosY(i) * ys;
             particleSystem.mBalls[i].setTranslationX(x);
             particleSystem.mBalls[i].setTranslationY(y);
+        }
+
+        final int count2 = particleSystem.getParticleCount2();
+        for (int i = 0; i < count2; i++) {
+            /*
+             * We transform the canvas so that the coordinate system matches
+             * the sensors coordinate system with the origin in the center
+             * of the screen and the unit is the meter.
+             */
+            final float x = xc + particleSystem.getPosX2(i) * xs;
+            final float y = yc - particleSystem.getPosY2(i) * ys;
+            particleSystem.mMonsters[i].setTranslationX(x);
+            particleSystem.mMonsters[i].setTranslationY(y);
         }
 
         // and make sure to redraw asap
